1c4d618bc91c11d2a36b1bdc3f211aca88032d4b
YuvrajKatkar <115986431+YuvrajKatkar@users.noreply.github.com>
2023-04-03 08:47:58 +0530

    Create PetersonNumber.java

---
+++ NumberProgram/PetersonNumber.java
@@ -0,0 +1,33 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class PetersonNumber {
+	static int fact(int num) {
+		int fact=1;
+		for(int i=1;i<=num;i++) {
+			fact = fact*i;
+		}
+		return fact;
+	}
+	public static boolean checkPeterson(int num) {
+		int sum = 0;int num1 = num;
+		while(num>0) {
+			sum += fact(num%10);
+			num/=10;
+		}
+		
+		if(sum!=num1) {
+			return false;
+		}
+		
+		return true;
+	}
+	public static void main(String[] args) {
+		System.out.println("Enter a number: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(checkPeterson(num)?"Number is Peterson":"Number is not Peterson");
+}
+	
+}
