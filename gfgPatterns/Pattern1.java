c1ea1436a163dd7d00b857d08fd8108027bfa426
Yuvraj Katkar <115986431+YuvrajKatkar@users.noreply.github.com>
2023-05-15 00:10:21 +0530

    Update Pattern1.java

--- gfgPatterns/Pattern1.java
+++ gfgPatterns/Pattern1.java
@@ -1,1 +1,17 @@
-asdf
+package patterns;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		int start=4;
+		for (int i = 1; i <= 5; i++) {
+			
+			for(int j = start;j>=1;j--) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i+" ");
+			}
+			start--;System.out.println();
+		}
+	}
+}
